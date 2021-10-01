2ab6a3480f91bb74c736a1a982a5d072a67e4cc7
DaleG18 <dgraha40@uncc.edu>
2021-09-30 20:06:34 -0400

    Create calculator_try.js

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,21 @@
+var x = window.prompt("Enter first number?");
+    var y = window.prompt("Enter second number?");
+
+function add(){
+    addNum = x + y 
+    alert(addNum);
+}
+
+function subtract(){
+    subNum = x - y 
+    alert(subNum);
+}
+
+function multiply(){
+    mulNum = x * y;
+    alert(mulNum);
+}
+function divide(){
+    divNum = x / y;
+    alert(divNum);
+}
