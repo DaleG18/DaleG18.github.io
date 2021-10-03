e66208dd67dd3376c0eed07e9bf4f4a45581c25e
DaleG18 <dgraha40@uncc.edu>
2021-10-02 21:45:03 -0400

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,6 +1,6 @@
 var x = window.prompt("Enter first number?");
 var y = window.prompt("Enter second number?");
-
+validateEntry(x,y);
 function add()    {
     addNum = +x + +y;
     alert(addNum);}
@@ -18,3 +18,8 @@ function multiply(){
 function divide() {
     divNum = x / y;
     alert(divNum);}
+
+function validateEntry(x,y) {
+    if (isNaN(x) || isNaN(y))  // if x or y are not numbers return a error message
+    {alert("These values do not work, please refresh to try again :)");}
+                            }
