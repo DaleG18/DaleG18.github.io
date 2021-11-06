e936c5d4b03138decc631cf60bc5b7b03a4e17e9
DaleG18 <dgraha40@uncc.edu>
2021-11-05 22:07:46 -0400

    Create form.js

---
+++ itis3135/brand/scripts/form.js
@@ -0,0 +1,4 @@
+function send(){
+    alert("Thank you for submitting! Your name is " + fullName.value + "\n Phone Number " + phoneNum.value
+    + "\n Email: " + email.value + "\n You heard of us from " + hear.value);
+}
