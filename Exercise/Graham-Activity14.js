f191ecfd4578eefcfcc0988578f817c458788075
DaleG18 <dgraha40@uncc.edu>
2021-11-20 00:09:14 -0500

    Update Graham-Activity14.js

--- Exercise/Graham-Activity14.js
+++ Exercise/Graham-Activity14.js
@@ -12,4 +12,43 @@ $(document).ready(function() {
             })
         })
     });
+    $.getJSON("json_files/sorkin.json",function(data){
+        $.each(data,function(){
+            $.each(this,function(key, value){
+                $("#sorkin"),append(
+                    "Month: " + value.month + "<br>" + 
+                    value.title + "<br>" +
+                    "Speaker: " + value.speaker + "<br>" +
+                    value.image + "<br>" + 
+                    value.text + "<br><br>"
+                );
+            })
+        })
+    });
+    $.getJSON("json_files/chua.json",function(data){
+        $.each(data,function(){
+            $.each(this,function(key, value){
+                $("#chua"),append(
+                    "Month: " + value.month + "<br>" + 
+                    value.title + "<br>" +
+                    "Speaker: " + value.speaker + "<br>" +
+                    value.image + "<br>" + 
+                    value.text + "<br><br>"
+                    );
+            })
+        })
+    });
+    $.getJSON("json_files/sampson.json",function(data){
+        $.each(data,function(){
+            $.each(this,function(key, value){
+                $("#sampson"),append(
+                    "Month: " + value.month + "<br>" + 
+                    value.title + "<br>" +
+                    "Speaker: " + value.speaker + "<br>" +
+                    value.image + "<br>" + 
+                    value.text + "<br><br>"
+                    );
+            })
+        })
+    });
 }); // end ready
