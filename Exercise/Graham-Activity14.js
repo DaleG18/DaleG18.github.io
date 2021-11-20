113a5749cfd8dcb707f88c40e3e326bfdea4d2a9
DaleG18 <dgraha40@uncc.edu>
2021-11-20 00:03:40 -0500

    Update Graham-Activity14.js

--- Exercise/Graham-Activity14.js
+++ Exercise/Graham-Activity14.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
+	$.getJSON("json_files/toobin.json",function(data){
+        $.each(data,function(){
+            $.each(this,function(key, value){
+                $("#toobin"),append(
+                    "Month: " + value.month + "<br>" + 
+                    value.title + "<br>" +
+                    "Speaker: " + value.speaker + "<br>" +
+                    value.image + "<br>" + 
+                    value.text + "<br><br>"
+                );
+            })
+        })
+    });
 }); // end ready
