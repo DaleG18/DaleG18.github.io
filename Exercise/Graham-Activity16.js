10cbd468b8885c330a1013a26e9e66ed772d91b0
DaleG18 <dgraha40@uncc.edu>
2021-11-27 18:42:41 -0500

    Update Graham-Activity16.js

--- Exercise/Graham-Activity16.js
+++ Exercise/Graham-Activity16.js
@@ -5,9 +5,8 @@ var url = "http://api.flickr.com/services/feeds/photos_public.gne?"+
 $.getJSON(url, function(data){ 
     var html = ""; 
     $.each(data.items, function(i, item){ 
-        html += "<h2>" + item.title + "</h2>";   
-        html += "<img src=" + item.media.m + ">"; 
-        html += "<p></p>"; 
+        html += "<a><img src='" + item.media.m +
+         "'title='" + item.title + "'></a>";
     }); 
  
     $("#new_building").html(html); 
