0e3140f048b189b5d3748f421d1401ee4e940e7c
DaleG18 <dgraha40@uncc.edu>
2021-11-27 10:09:49 -0500

    Update Graham-Activity15.js

--- Exercise/Graham-Activity15.js
+++ Exercise/Graham-Activity15.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
-    $.ajax({ 
-        type: "get", 
-        url: "facultyList.json", 
-        beforeSend: function() {$("#faculty").html("Loading...");}, 
-        timeout: 10000, 
-        error: function(xhr, status, error) { 
-            alert("Error: " + xhr.status + " - " + error); 
-        }, 
-        dataType: "json", 
-        success: function(data) { 
-            $("#faculty").html(""); 
-            $(data).find("facultymembers").children().each(function() { 
-                var xmlDoc = $(this); 
-                $("#faculty").append("<img>" +  
-                    xmlDoc.find("image").text() + "</img>" +  
-                    xmlDoc.find("full_name").text() + "<br>" +  
-                    xmlDoc.find("department").text() + "<br>" +
-                    xmlDoc.find("bio").text() + "<br>");  
-            }); 
-        } 
-    }); 
-});
+    $.getJSON("facultyList.json", function(data) {
+        $.each(data, function() {
+        $.each(this, function(key, value) {
+        $("#facultymembers").append(
+            "<img src="+"'"+value.image+"'><br>"+
+        "<h3>" + value.full_name + "</h3>" +
+        "<h3>" + value.title + "</h3>" +
+        "<p>" + value.tag_line + "</p><br>"
+        );
+        });
+        });
+        });
+        });
