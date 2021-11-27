5e8733cf44fe9807d72e96dc0153fa20e80cb625
DaleG18 <dgraha40@uncc.edu>
2021-11-27 10:05:23 -0500

    Update Graham-Activity15.js

--- Exercise/Graham-Activity15.js
+++ Exercise/Graham-Activity15.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-
+    $.ajax({ 
+        type: "get", 
+        url: "facultyList.json", 
+        beforeSend: function() {$("#faculty").html("Loading...");}, 
+        timeout: 10000, 
+        error: function(xhr, status, error) { 
+            alert("Error: " + xhr.status + " - " + error); 
+        }, 
+        dataType: "json", 
+        success: function(data) { 
+            $("#faculty").html(""); 
+            $(data).find("facultymembers").children().each(function() { 
+                var xmlDoc = $(this); 
+                $("#faculty").append("<img>" +  
+                    xmlDoc.find("image").text() + "</img>" +  
+                    xmlDoc.find("full_name").text() + "<br>" +  
+                    xmlDoc.find("department").text() + "<br>" +
+                    xmlDoc.find("bio").text() + "<br>");  
+            }); 
+        } 
+    }); 
 });
