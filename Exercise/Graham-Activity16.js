aa527f10e5d5dcd4ec7bd1c1f523b07a57aee196
DaleG18 <dgraha40@uncc.edu>
2021-11-27 18:05:33 -0500

    Update Graham-Activity16.js

--- Exercise/Graham-Activity16.js
+++ Exercise/Graham-Activity16.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+var url = "http://api.flickr.com/services/feeds/photos_public.gne?"+
+"id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
 
+$.getJSON(url, function(data){ 
+    var html = ""; 
+    $.each(data.items, function(i, item){ 
+        html += "<h2>" + item.title + "</h2>";   
+        html += "<img src=" + item.media.m + ">"; 
+        html += "<p></p>"; 
+    }); 
+ 
+    $("#photos").html(html); 
+});           
 });
