f7764cfe785454f24c4b06f4d8a208e99d60d386
DaleG18 <dgraha40@uncc.edu>
2021-11-27 10:11:43 -0500

    Update Graham-Activity15.js

--- Exercise/Graham-Activity15.js
+++ Exercise/Graham-Activity15.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.getJSON("facultyList.json", function(data) {
         $.each(data, function() {
         $.each(this, function(key, value) {
-        $("#facultymembers").append(
+        $("#faculty").append(
             "<img src="+"'"+value.image+"'><br>"+
         "<h3>" + value.full_name + "</h3>" +
         "<h3>" + value.title + "</h3>" +
