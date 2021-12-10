8e2f07f1ff6e4ab1151586bd0ede183a9a71b63e
DaleG18 <dgraha40@uncc.edu>
2021-12-10 14:34:44 -0500

    Update services.js

--- itis3135/project/scripts/services.js
+++ itis3135/project/scripts/services.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $.each(data, function() {
         $.each(this, function(key, value) {
         $("#services").append(
-            "<img src="+"'"+value.image+"'><br>"+ '" width = 80 height = 80>' +
+            "<img src="+"'"+value.image+"'><br>"+
             "<h2>" + value.full_name + "</h2>" +
             "<h3>" + value.department + "</h3>" +
             "<h4>" + value.phone_number + "</h4>" +
