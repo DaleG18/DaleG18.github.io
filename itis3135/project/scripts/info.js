e6fc4b7dd8b5f7e675a7989c6baafa4b4a3c08cc
DaleG18 <dgraha40@uncc.edu>
2021-12-10 22:16:56 -0500

    Create info.js

---
+++ itis3135/project/scripts/info.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "info.json",
+        beforeSend: function() {
+            $("#info").html("Loading...");
+        },
+        
+        dataType: "json",
+        success: function(data) {
+            $("#team").html("");
+            $.each(data, function(){
+                $.each(this, function(key, value){
+                $("#info").append
+                ("<h3>"+ value.title+ "</h3>" +
+                "<p>" + value.pgraph +"</p> <br>"
+                )
+            })  
+            })
+        }
+    });
+});
