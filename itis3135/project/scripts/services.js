c87843e83dc4bcf0518b70ae4c080d938cb2ec57
DaleG18 <dgraha40@uncc.edu>
2021-12-11 00:09:05 -0500

    Update services.js

--- itis3135/project/scripts/services.js
+++ itis3135/project/scripts/services.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $.each(data, function() {
         $.each(this, function(key, value) {
         $("#services").append(
-            "<img src="+"'"+value.image+"'><br>"+
+            "<img src="+"'"+value.image+"'><br>"+ //all text and images are formatted in descending order h3-h5
             "<h3>" + value.full_name + "</h3>" +
             "<h4>" + value.department + "</h4>" +
             "<h5>" + value.phone_number + "</h5>" +
