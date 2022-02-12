680100ebd968a05b62a89644b28574e236c062b6
DaleG18 <dgraha40@uncc.edu>
2022-02-11 21:03:43 -0500

    enter

--- GrahamDaleExercise2/app.js
+++ GrahamDaleExercise2/app.js
@@ -1,24 +1,16 @@
-const  http = require('http')
-const fs = require('fs');
+const http = require('http');
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method);
+const server = http.createServer((req, res) => {
+console.log(req.url, req.method);
 
-res.setHeader('Content-Type', 'text/plain');
-let path = './views/';
+res.setHeader('Content-Type', 'text/html');
 
-fs.readFile('./views/index.html', (err,data) =>{
-    if(err){
-        console.log(err);
-        res.end();
-       
-        } else{
-            res.write(data);
-            res.end();
-    }
+res.write('<head><link rel = "stylesheet" href ="#"></head>');
+res.write('<p>hello niggas</p>');
+res.write('<p>hello again, niggas</p>');
+res.end();
 });
 
-});
 server.listen(3000, 'localhost', () =>{
-    console.log('losyening for requests on port 3000');
-})
+    console.log('listening for requests on port 3000');
+});
