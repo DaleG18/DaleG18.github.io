f34e5649a71e353754e8269cbb1ac8a5a84dcda0
DaleG18 <dgraha40@uncc.edu>
2022-02-11 20:56:29 -0500

    enter

--- GrahamDaleExercise2/app.js
+++ GrahamDaleExercise2/app.js
@@ -6,4 +6,19 @@ const server = http.createServer((req,res) => {
 
 res.setHeader('Content-Type', 'text/plain');
 let path = './views/';
+
+fs.readFile('./views/index.html', (err,data) =>{
+    if(err){
+        console.log(err);
+        res.end();
+       
+        } else{
+            res.write(data);
+            res.end();
+    }
+});
+
+});
+server.listen(3000, 'localhost', () =>{
+    console.log('losyening for requests on port 3000');
 })
