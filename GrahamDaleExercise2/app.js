f80215988312c4d6ec9c8f58f1ae6b3f8958acf4
DaleG18 <dgraha40@uncc.edu>
2022-02-11 20:36:07 -0500

    enter

---
+++ GrahamDaleExercise2/app.js
@@ -0,0 +1,36 @@
+const  http = require('http')
+const fs = require('fs');
+const { isAbsolute } = require('path/posix');
+
+const port = 8080;
+const host = 'localhost';
+const server = http.createServer((req,res)=> {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+let path = './views/';
+
+if(req.url === '/contact'){
+    path = path + 'contact.html';
+}
+else if(req.url === '/about'){
+    path = path + 'about.html';
+}
+else{
+    res.statusCode = 404;
+    path = path + '404.html';
+}
+fs.readFile('./about.html', (err, data) =>{
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.writeFile(data);
+            res.end();
+        }
+});
+});
+
+server.listen(port, host, () =>{
+    console.log('The server is running on port', port)
+});
