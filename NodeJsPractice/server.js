a9e61c43462b8e234f22d2fe375a7988559af7a4
DaleG18 <dgraha40@uncc.edu>
2022-02-11 21:16:50 -0500

    enter

--- NodeJsPractice/server.js
+++ NodeJsPractice/server.js
@@ -1,14 +1,33 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
 console.log(req.url, req.method);
 
 res.setHeader('Content-Type', 'text/html');
+let path = './views';
+switch(req.url){
+    case '/':
+        path += 'index.html';
+        break;
+    case '/about':
+        path+= 'about.html';
+        break;
+    default: 
+        path += '404.html';
+        break;
+}
 
-res.write('<head><link rel = "stylesheet" href ="#"></head>');
-res.write('<p>hello niggas</p>');
-res.write('<p>hello again, niggas</p>');
-res.end();
+fs.readFile(path, (err, data) =>{
+if(err){
+    console.log(err);
+    res.end();
+}
+else{
+    //res.write(data);
+    res.end(data);
+}
+});
+
 });
 
 server.listen(3000, 'localhost', () =>{
